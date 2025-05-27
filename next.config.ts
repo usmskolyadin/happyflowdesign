92e548e585fb930a8493703ae4dd1ce7ef78c6ab
usmskolyadin <koladinmaksim246@gmail.com>
2025-05-27 19:30:13 +0300

    update netlify.toml

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', 
-  
-  images: {
-    unoptimized: true // если не используете Image Optimization
-  }
 };
 
 export default nextConfig;
